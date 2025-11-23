0fe1270cd0ce2372179e1b8d500d88ada9f515d8
Sadman Sakib <96833985+Sadman-Sakib56@users.noreply.github.com>
2025-11-23 15:14:51 +0600

    fourth problem solved
    Added a filter function to return items with a rating of 4 or higher.

--- solution.ts
+++ solution.ts
@@ -38,11 +38,20 @@ class Person {
     }
 }
 const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
 const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
 
 
 
+function filterByRating(items: {title: string; rating: number} []): {title: string; rating: number} [] {
+
+    const result:{title: string; rating: number} [] = [];
+
+    for(const item of items) {
+        if(item.rating >= 4){
+            result.push(item);
+        }
+    }
+
+    return result;
+}
 
